00a8ac1eebce023162274e6610697b5b4265a130
Amar Maruf <amarmaruf2403@gmail.com>
2022-06-11 19:33:41 +0700

    memperbaiki bug

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 var menuBtn = document.getElementById("menuBtn");
 var sideNav = document.getElementById("sideNav");
-var homeBtn = document.getElementById("homeBtn")
+var homeBtn = document.getElementById("homeBtn");
 
-sideNav.style.paddingRight = "-250px";
-menuBtn.onclick = function() {
-    if (sideNav.style.paddingRight == "-250px") {
-        sideNav.style.paddingRight = "0";
-    } else {
-        sideNav.style.paddingRight = "-250px";
-    }
-}
-var scroll = new SmoothScroll('a[href*="#"', {
+sideNav.style.right = "-250px";
+menuBtn.onclick = function () {
+  if (sideNav.style.right == "-250px") {
+    sideNav.style.right = "0";
+  } else {
+    sideNav.style.right = "-250px";
+  }
+};
+var scroll =
+  ('a[href*="#"',
+  {
     speed: 1000,
-    speedAsDuration: true
-});
+    speedAsDuration: true,
+  });
